40c54b661b4040355ce5768e83628a078cf06f49
ceyda.yilmaz <ceyda.yilmaz@CEYDAYILMAZPC>
2024-08-14 17:30:19 +0300

    Proje dosyası ekle.

---
+++ Abstraction/Calisan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        public Calisan(string ad, string soyad, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+        }
+
+        // Abstract method
+        public abstract void Gorev();
+    }
+
+    class YazilimGelistirici : Calisan
+    {
+        public YazilimGelistirici(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}: Yazılım geliştirici olarak çalışıyorum.");
+        }
+    }
+
+    class ProjeYoneticisi : Calisan
+    {
+        public ProjeYoneticisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}: Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+
+    class SatisTemsilcisi : Calisan
+    {
+        public SatisTemsilcisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}: Satış temsilcisi olarak çalışıyorum.");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using Abstraction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Çalışanları tanımlıyoruz
+        Calisan yazilimci = new YazilimGelistirici("Ahmet", "Yılmaz", "Yazılım");
+        Calisan projeYoneticisi = new ProjeYoneticisi("Hasan", "Çıldırmış", "Proje Yönetimi");
+        Calisan satisTemsilcisi = new SatisTemsilcisi("Ayşe", "Kara", "Satış");
+
+        // Her çalışanın görevini çağırıyoruz
+        yazilimci.Gorev();
+        projeYoneticisi.Gorev();
+        satisTemsilcisi.Gorev();
+
+        Console.ReadKey();
+    }
+}
